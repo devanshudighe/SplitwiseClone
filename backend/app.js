fce114d5a7c90dab150e439f574df0aaef030206
Checkout <devanshu.dighe@sjsu.edu>
2021-02-12 04:23:13 -0800

    Project Init

---
+++ backend/app.js
@@ -0,0 +1,35 @@
+//import express module 
+var express = require('express');
+//create an express app
+var app = express();
+//require express middleware body-parser
+var bodyParser = require('body-parser');
+//require express session
+var session = require('express-session');
+var cookieParser = require('cookie-parser');
+var cors = require('cors');
+
+app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
+
+
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
+    res.setHeader('Cache-Control', 'no-cache');
+    next();
+  });
+//use body parser to parse JSON and urlencoded request bodies
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+//use cookie parser to parse request headers
+app.use(cookieParser());
+//use session to store user data between HTTP requests
+app.use(session({
+    secret: 'splitwise',
+    resave: false,
+    saveUninitialized: true
+}));
+
+module.exports = app;
