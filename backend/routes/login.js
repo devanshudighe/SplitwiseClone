6c3ab0565257c1da4589d70c2ba611dc1e388036
Checkout <devanshu.dighe@sjsu.edu>
2021-02-15 20:30:46 -0800

    Login Signup Route

---
+++ backend/routes/login.js
@@ -0,0 +1,57 @@
+var express = require('express');
+var router = express.Router();
+var sql = require('../sql')
+//route to Login Page
+// router.get('/login', function (req, res) {
+//     //check if user session exits
+//     if (req.session.user) {
+//         res.status(200).send("In Dashboard")
+//     } else{
+//         res.status(200).send("Invalid Creds")
+//         // invalidCredentials = false
+//         // res.st('login',{
+//         //     invalidCredentials : invalidCredentials
+//         // });
+//     }
+        
+// });
+// var invalidCredentials = false
+router.post('/', async function (req, res) {
+        // console.log("Req Body : ", req.body);
+        let userDetails = `CALL getUserInfo('${req.body.email}');`
+
+        sql.query(userDetails, (err,result) => {
+            if (err) {
+                res.writeHead(500, {
+                  'Content-Type': 'text/plain'
+                });
+                res.send("Database Error");
+              }
+            console.log(result)
+            if (result && result.length > 0){
+                if(req.body.password === result[0][0].password){
+                    let user = {
+                        userId : result[0][0].user_id,
+                        user_name : result[0][0].name,
+                        email : result[0][0].email,
+                        currency : result[0][0].currency,
+                        language : result[0][0].language,
+                        phone : result[0][0].phone,
+                        timeZone : result[0][0].timeZone
+                    }
+                    res.status(200).send(user)
+                }
+                else{
+                    res.status(200).send("Wrong Password")
+                }
+            }
+            else {
+                res.writeHead(401, {
+                    'Content-Type': 'text/plain'
+                })
+                res.end("NO_USER");
+            }
+        })
+});
+
+module.exports = router;
---
+++ backend/routes/signup.js
@@ -0,0 +1,50 @@
+var express = require('express');
+var router = express.Router();
+var sql = require('../sql')
+// Signup page
+// var duplicateUser = false
+// router.get('/signup', function (req, res) {
+//     if (req.session.user) {
+//         res.redirect('/dashboard');
+//     } else {
+//         duplicateUser = false
+//         res.send('signup', {
+//             duplicateUser: duplicateUser
+//         });
+//     }
+// });
+
+
+router.post('/', async function (req, res) {
+    console.log("Signup post req");
+    let details = `CALL storeUserInfo('${req.body.email}','${req.body.user_name}','${req.body.password}');`
+
+    sql.query(details, (err,result) => {
+        if (err) {
+            res.writeHead(500, {
+                'Content-Type': 'text/plain'
+            });
+            res.end("Database Error");
+            }
+        if(result && result.length > 0 && result[0][0].flag){
+            res.writeHead(200, {
+                'Content-Type': 'text/plain'
+                });
+            res.end("User is Added");
+        }
+        else if(result && result.length > 0 && !result[0][0].flag){
+            res.writeHead(500, {
+                'Content-Type': 'text/plain'
+                });
+            res.end("User Already Exists");
+        }
+        else{
+            res.writeHead(401, {
+                'Content-Type': 'text/plain'
+            })
+            res.end("NO_DATA");
+        }
+    })
+});
+
+module.exports = router;
