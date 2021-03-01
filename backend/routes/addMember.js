3c3c2a2148cac4a4a7b0a0f5b03ea440825e5738
Checkout <devanshu.dighe@sjsu.edu>
2021-03-01 15:05:05 -0800

    Adding member to a group

---
+++ backend/routes/addMember.js
@@ -0,0 +1,50 @@
+var express = require('express');
+var router = express.Router();
+var sql = require('../sql')
+
+router.post('/', function (req, res) {
+    let createGroups = `CALL newMemberInvitation('${req.body.email}','${req.body.groupName}')`
+
+    sql.query(createGroups, (err, result) => {
+        if (err) {
+            res.writeHead(500, {
+                'Content-Type': 'text/plain'
+            });
+            res.end("Database Error");
+        }
+        if (result && result.length > 0 && result[0][0].flag == 'MEMBER_DOES_NOT_EXIST') {
+            res.writeHead(500, {
+                'Content-Type': 'text/plain'
+                });
+            res.end('Member not on Splitwise')
+        }
+        else if(result && result.length > 0 && result[0][0].flag == 'GROUP_DOES_NOT_EXIST') {
+            res.writeHead(500, {
+                'Content-Type': 'text/plain'
+                });
+            res.end('Group is not present')
+        }
+        else if(result && result.length > 0 && result[0][0].flag == 'MEMBER_ALREADY_INVITED') {
+            res.writeHead(500, {
+                'Content-Type': 'text/plain'
+                });
+            res.end('Person is already invited')
+            
+        }
+        else if(result && result.length > 0 && result[0][0].flag == 'MEMBER_INVITED') {
+            res.writeHead(200, {
+                'Content-Type': 'text/plain'
+                });
+            res.end('Invitation Sent')
+            
+        }
+        else{
+            res.writeHead(401, {
+                'Content-Type': 'text/plain'
+                });
+            res.end('No_Data')
+        }
+    })
+});
+
+module.exports = router;
